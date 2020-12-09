e44d56b41fe4df7b8f0bbd4cd68d487feb00d6bd
Damian Filipek <damianfilipek81@gmail.com>
2020-12-09 15:40:37 +0100

    Add clicked article is active-clickedElement function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 'use strict';
 
 const titleClickHandler = function (event) {
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
 
-/* remove class 'active' from all article links  */
+/* [Done] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 /* add class 'active' to the clicked link */
-
-/* remove class 'active' from all articles */
+clickedElement.classList.add('active');
+console.log('clickedElement (with plus): ' + clickedElement);
+/* [Done] remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.posts .active');
 
 for(let activeArticle of activeArticles){
